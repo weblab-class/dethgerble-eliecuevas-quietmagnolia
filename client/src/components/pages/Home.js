a06e55993da501070490ff8db41dafb763d71615
Jake Jones <jakejones@Jakes-2020-MacBook-Air.local>
2022-01-13 18:55:35 -0500

    deleted skeleton and moved login stuff to home

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "./Skeleton.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID (DONE - Elie)
+const GOOGLE_CLIENT_ID = "65119842375-qsvmlu1p97jbaccqj2hlgji2qaeesp7i.apps.googleusercontent.com";
+
+const Home = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <>
+      {userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
