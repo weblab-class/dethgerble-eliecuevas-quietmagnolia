c33a5755ef41f3da0f439de031d1ce40de6d751b
Jake Jones <jakejones@Jakes-2020-MacBook-Air.local>
2022-01-16 19:41:24 -0500

    fixed bug with button menu

--- client/src/components/modules/ButtonMenu.js
+++ client/src/components/modules/ButtonMenu.js
@@ -9,10 +9,10 @@ const ButtonMenu = () => (
     <li> <a href="/">Welcome</a> </li>
     <li><a href="/tasks">Tasks</a></li>
     <li><a href="/farm">Farm</a></li>
-    <li><a href="/stats">Stats</a></li>
     <li><a href="/friends">Friends</a></li>
+    <li><a href="/stats">Stats</a></li>
+    <li><a href="/profile">Profile</a></li>
     <li><a href="/about">About</a></li>
-    <li><a href="/profile">About</a></li>
   </ul>
  </div>
 </nav>
